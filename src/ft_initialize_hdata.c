c712a8553351e857066a712bf7d7255c91da217e
thatkstho <othmanehachim@gmail.com>
2019-10-06 17:44:34 +0100

    initialize proporly initializes all data variables

--- src/ft_initialize_hdata.c
+++ src/ft_initialize_hdata.c
@@ -6,7 +6,7 @@
 /*   By: ohachim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/06 11:25:38 by ohachim           #+#    #+#             */
-/*   Updated: 2019/10/06 11:25:51 by ohachim          ###   ########.fr       */
+/*   Updated: 2019/10/06 17:42:43 by ohachim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 void	ft_initialize_hdata(t_data *data)
 {
+	data->ants = 0;
+	data->vertices = 0;
+	data->max_hash = 0;
+	data->queue = 0;
+	data->paths = 0;
 	data->hash_table = NULL;
 	data->start = NULL;
 	data->end = NULL;
